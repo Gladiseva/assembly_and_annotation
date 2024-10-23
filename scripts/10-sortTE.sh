78b57c74f9e7dc1c0afdcae130a015dbdbed820d
gladiseva <lollija.lukasova@gmail.com>
2024-10-23 10:41:27 +0200

    modify tesorter

---
+++ scripts/10-sortTE.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+  
+#SBATCH --cpus-per-task=20
+#SBATCH --mem=40G
+#SBATCH --time=1-00:00:00
+#SBATCH --job-name=TEsorter
+#SBATCH --partition=pibu_el8
+
+ASSEMBLY_DIR=/data/users/lgladiseva/assembly_course/assembly_out/lja/assembly.fasta
+WORK_DIR=/data/users/lgladiseva/annotation_course
+CONTAINER_DIR=/data/courses/assembly-annotation-course/containers2/TEsorter_1.3.0.sif
+
+cd $WORK_DIR/EDTA_annotation2
+
+apptainer exec -C -H $WORK_DIR -H ${pwd}:/work \
+--writable-tmpfs -u $CONTAINER_DIR TEsorter assembly.fasta.mod.EDTA.intact.fa -db rexdb-plant
