e5612111d710963d0848c0eca26fc7d693cfc14f
gladiseva <lollija.lukasova@gmail.com>
2024-11-08 12:40:14 +0100

    fix plots, fix BUSCO

--- scripts/26-busco_plots.sh
+++ scripts/26-busco_plots.sh
@@ -17,8 +17,8 @@ module load BUSCO/5.4.2-foss-2021a
 module load SeqKit/2.6.1
 
 ## create busco plot
-cp $OUT_DIR/transcripts/short_summary.specific.brassicales_odb10.transcripts.txt $PLOT_DIR/.
-cp $OUT_DIR/proteins/short_summary.specific.brassicales_odb10.proteins.txt $PLOT_DIR/.
+cp /data/users/lgladiseva/annotation_course/scripts/data/users/lgladiseva/annotation_course/gene_annotation/evaluation/transcripts/short_summary.specific.brassicales_odb10.transcripts.txt $PLOT_DIR/.
+cp /data/users/lgladiseva/annotation_course/scripts/data/users/lgladiseva/annotation_course/gene_annotation/evaluation/proteins/short_summary.specific.brassicales_odb10.proteins.txt $PLOT_DIR/.
 
 cd $PLOT_DIR
 wget https://gitlab.com/ezlab/busco/-/raw/master/scripts/generate_plot.py
